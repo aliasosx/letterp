8bebfbb25849492b3b9decd241e6403908b365ac
alisaosx <aliasosx@hotmail.com>
2018-08-01 16:38:52 +0700

    add product categories

---
+++ app/controllers/api/v1/product_categories_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::ProductCategoriesController < ApplicationController
+    def index()
+        render json: ProductCategory.all()
+    end
+    def create()
+        puts product_category_params[:prod_cat_code]
+        @productCat = ProductCategory.create(prod_cat_code:product_category_params[:prod_cat_code],
+            prod_cat_name_en:product_category_params[:prod_cat_name_en],prod_cat_name_la:product_category_params[:prod_cat_name_la])
+        
+        render json: {success: 'ok'}
+    end
+    private 
+    def product_category_params()
+        params.permit(:id,:prod_cat_code,:prod_cat_name_en,:prod_cat_name_la)
+    end
+end
