855ef458110fb1476b5c4c31d5a27a44c79020c1
alisaosx <aliasosx@hotmail.com>
2018-09-19 08:30:21 +0700

    reformat code

--- app/controllers/api/v1/menus_controller.rb
+++ app/controllers/api/v1/menus_controller.rb
@@ -8,6 +8,7 @@ class Api::V1::MenusController < ApplicationController
         user  = UserMaster.find_by(user_id: menu_params[:id])        
         tranx_role  = TranxRole.find_by(user_id: user.user_id)
         tranx_menu = TranxMenu.where(role_id:tranx_role.role_id )
+        
         profile = { user: user, role: tranx_role , menues: tranx_menu }        
         render json: profile
     end
